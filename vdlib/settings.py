765ef6005f37f6d9d008c11776e24506330821bb
Auwais Hussain <hussa15033@gmail.com>
2023-09-01 23:24:28 +0100

    Improve styling on login and register views
    - Use widget-tweaks to include classes on input fields

--- vdlib/settings.py
+++ vdlib/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'simplelib'
+    'simplelib',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'simplelib.user'
+
+LOGIN_URL = 'login'
